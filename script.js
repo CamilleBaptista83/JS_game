b850ee1d7795742fc226dbc8782407cf7ae9db3b
Camille <cambaptista83@gmail.com>
2021-12-19 21:29:02 +0100

    Payer 1 fonctionnel / player 2 ne se supprime pas au reset

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ var buttonRoll = document.getElementById('buttonRoll');
 var buttonHold = document.getElementById('buttonHold');
 var buttonReset = document.getElementById('buttonReset');
 
-
 let player = 1
 
 let dice = 0
@@ -10,18 +9,39 @@ let dice = 0
 let playerOneRound = 0
 let playerOneGlobal = 0
 
+let playerTwoRound = 0
+let playerTwoGlobal = 0
 
-if (player = 1) {
-  function rollDice() {
+// function
+
+function rollDice() {
+  if (player == 1) {
     dice = Math.floor(Math.random() * (6 - 1) + 1);
     playerOneRound = playerOneRound + dice
     playerOneGlobal = playerOneGlobal
-    updateDisplay(dice, playerOneRound, playerOneGlobal)
+    updateDisplay(1, dice, playerOneRound, playerOneGlobal)
+  } else {
+    dice = Math.floor(Math.random() * (6 - 1) + 1);
+    playerTwoRound = playerTwoRound + dice
+    playerTwoGlobal = playerTwoGlobal
+    updateDisplay(2, dice, playerTwoRound, playerTwoGlobal)
   }
-  function hold() {
+}
+
+
+function hold() {
+  if (player == 1) {
     playerOneGlobal = playerOneRound + playerOneGlobal
     playerOneRound = 0
-    updateDisplay(0, playerOneRound, playerOneGlobal)
+    dice = 0
+    updateDisplay(1, dice, playerOneRound, playerOneGlobal)
+    player = 2
+  } else {
+    playerTwoGlobal = playerTwoRound + playerTwoGlobal
+    playerTwoRound = 0
+    dice = 0
+    updateDisplay(2, dice, playerTwoRound, playerTwoGlobal)
+    player = 1
   }
 
 }
@@ -30,17 +50,26 @@ function reset() {
   dice = 0
   playerOneRound = 0
   playerOneGlobal = 0
-  updateDisplay(dice, playerOneRound, playerOneGlobal)
-
+  playerTwoRound = 0
+  playerTwoGlobal = 0
+  updateDisplay(1, dice, playerOneRound, playerOneGlobal)
 }
 
 buttonRoll.addEventListener('click', rollDice);
 buttonHold.addEventListener('click', hold);
 buttonReset.addEventListener('click', reset);
 
-function updateDisplay(diceValue, roundValue, globalValue) {
-  document.getElementById("dice").innerHTML = diceValue;
-  document.getElementById("round").innerHTML = roundValue;
-  document.getElementById("global").innerHTML = globalValue;
+function updateDisplay(player, diceValue, roundValue, globalValue) {
+  if (player == 1) {
+    document.getElementById("dice-one").innerHTML = diceValue;
+    document.getElementById("round-one").innerHTML = roundValue;
+    document.getElementById("global-one").innerHTML = globalValue;
+  } else { if(player == 2) {
+    document.getElementById("dice-two").innerHTML = diceValue;
+    document.getElementById("round-two").innerHTML = roundValue;
+    document.getElementById("global-two").innerHTML = globalValue;
+  }
+
+  }
 
 }
